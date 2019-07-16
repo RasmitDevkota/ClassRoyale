8ec6489b903e611118a8642aab749682968ed296
verycoolmathgames <52218872+verycoolmathgames@users.noreply.github.com>
2019-07-16 14:33:22 -0400

    sign in stuff

---
+++ app/src/main/java/com/alientech/classroyale/EmailPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.alientech.classroyale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emailpassword)
+    }
+}
