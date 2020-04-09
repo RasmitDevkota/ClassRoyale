e56f5b274c85888c20221a7423d532aa40d56a78
Dr AlienTech <alientechgamig@hotmail.com>
2020-04-09 18:18:02 -0400

    Gitacora saved 12 changed files

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const firebase = require('firebase');
+const functions = require('firebase-functions');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDMq0mi1Se1KXRyqaIwVZnv1csYshtrgu0",
+    authDomain: "coronavirusbot19.firebaseapp.com",
+    databaseURL: "https://coronavirusbot19.firebaseio.com",
+    projectId: "coronavirusbot19",
+    storageBucket: "coronavirusbot19.appspot.com",
+    messagingSenderId: "814043085257",
+    appId: "1:814043085257:web:d4151d18cb5d4a16ca1018",
+    measurementId: "G-4TKZD7504L"
+});
+
+const db = firebase.firestore();
+
+exports.startGame = functions.https.onRequest((req, res) => {
+    
+});
