69d562b3cb14c247a6aa0346a5ad895440e594bd
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-03 16:55:47 -0400

    Gitacora saved 12 changed files

--- functions/index.js
+++ functions/index.js
@@ -39,7 +39,7 @@ exports.userJoin = functions.firestore.document('games/{gameid}').onUpdate((chan
             console.error("(39) " + e);
         });
     }).catch(function (e) {
-        console.log("(49)" + e);
+        console.log("(42)" + e);
     });
 });
 
