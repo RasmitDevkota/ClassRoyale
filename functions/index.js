07110ba86c0c31b6c2147036fca3ac9ee89465b4
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-03 12:32:35 -0400

    Gitacora saved 12 changed files

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,14 @@ const firebase = require('firebase');
 const functions = require('firebase-functions');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDMq0mi1Se1KXRyqaIwVZnv1csYshtrgu0",
-    authDomain: "coronavirusbot19.firebaseapp.com",
-    databaseURL: "https://coronavirusbot19.firebaseio.com",
-    projectId: "coronavirusbot19",
-    storageBucket: "coronavirusbot19.appspot.com",
-    messagingSenderId: "814043085257",
-    appId: "1:814043085257:web:d4151d18cb5d4a16ca1018",
-    measurementId: "G-4TKZD7504L"
+    apiKey: "AIzaSyDz4E7cWDIMedScuc4TvGdEawhWGOLn4SQ",
+    authDomain: "class-royale.firebaseapp.com",
+    databaseURL: "https://class-royale.firebaseio.com",
+    projectId: "class-royale",
+    storageBucket: "class-royale.appspot.com",
+    messagingSenderId: "670502096879",
+    appId: "1:670502096879:web:d06e9f32378e0296fbf01a",
+    measurementId: "G-F4CZ8W9P2B"
 });
 
 var db = firebase.firestore();
