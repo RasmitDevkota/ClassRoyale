1f76d1689757e0f8d08165e2dfb543167b2b84f9
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-03 17:26:07 -0400

    Gitacora saved 12 changed files

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ exports.userJoin = functions.firestore.document('games/{gameid}').onUpdate((chan
         console.log()
 
         if (!queue) {
-            return;
+            return 0;
         }
 
         for (uid in queue) {
