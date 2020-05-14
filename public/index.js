bbb86fd24667916e85d399093a25b85bbd3702be
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:22:20 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -68,7 +68,7 @@ function addCard() {
             break;
     }
 
-    console.log(`${name}, ${HP}, ${}`)
+    console.log(`${name}, ${HP}, ${description}`)
 
     cards.collection(type).doc(name).get().then(function (doc) {
         if (!doc.exists) {
