fe4f21c6ee434042ab8be6ec5e7cadc520c2d163
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:30:05 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -25,7 +25,7 @@ function addCard() {
     var rarity = document.getElementById("cardRarity").value;
     var thumbnail = document.getElementById("cardThumbnail");
 
-    if (!name || !type || !des`)
+    if (!name || !type || !description || !)
 
     var seed = Math.round(Math.random());
     var HPValues = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600];
