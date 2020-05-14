1119f15fb8888e1821a09938d96a7f4b7bac42a4
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 11:38:10 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -16,7 +16,7 @@ var users = db.collection("users");
 var emails = db.collection("emails");
 
 var storage = firebase.storage().ref();
-var 
+var cardI
 
 function addCard() {
     var name = document.getElementById("cardName").textContent;
