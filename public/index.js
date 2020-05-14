0ff4487bba562c40ef4363f56d79b47b59229dfb
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:53:17 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -37,7 +37,7 @@ function addCard() {
     var thumbnail = document.getElementById("cardThumbnail").files[0];
 
     if (!name || !type || !description || !rarity) {
-        alert ("You forgot to enter an input for something! Every field is required! ");
+        alert ("You forgot to enter an input for something! Every field is required!" + name + type + description + type + rarity);
         return console.log("Not enough parameters given.");
     }
 
