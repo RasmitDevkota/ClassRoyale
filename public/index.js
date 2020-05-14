11997dabb4af394f2eef3ace27d77fefe2c67275
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 14:25:02 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -38,7 +38,7 @@ function addCard() {
 
         var storageRef = firebase.storage.ref('card_thumbnails/' + file.name);
         storageRef.put(file).then(function (snapshot) {
-            console.log('Uploaded file' + file.name + '!');
+            console.log('Uploaded file ' + file.name + '!');
         });
     });
 
