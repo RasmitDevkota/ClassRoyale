51d40ebef5bdc81f88c314dcacba0bbc98d55d3a
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 14:46:42 -0400

    Gitacora saved 3 changed files

--- public/index.js
+++ public/index.js
@@ -41,7 +41,7 @@ function addCard() {
         ['Legendary', 8],
         ['Mythical', 10]
     ]);
-    var seedOffset = seedOffsets.get()
+    var seedOffset = seedOffsets.get(rarity)
     
     switch (rarity) {
         case "Common":
