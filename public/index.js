bfe65d8b9d72961de2055db55f4fdf25d81b3dc0
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:02:52 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -28,7 +28,7 @@ function addCard() {
     TODO("Use rarity to generate HP, attackDamage, and xpToLevelUp");
 
     var HP = 0;
-    
+    var 
 
     cards.collection(type).doc(name).get().then(function (doc) {
         if (!doc.exists) {
