1d99bb1e812c83d6c360e58a8e7e9f05a1cbabab
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:28:54 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -86,6 +86,23 @@ function addCard() {
         if (!doc.exists) {
             if (["Person", "Normal"].includes(type)) {
 
+                cards.collection(type).doc(name).set({
+                    name: name,
+                    HP: HP,
+                    type: type,
+                    description: description,
+                    rarity: rarity,
+                    level: 1,
+                    attackDamage: attackDamage,
+                    XP: 0,
+                    xpToLevelUp: 1000
+                }).then(function () {
+                    console.log("Successfully added card ${name} of type ${type}!");
+                    return alert("Successfully added card ${name} of type ${type}!");
+                }).catch(function (e) {
+                    console.log(e);
+                    return alert("Error occurred! Please contact a developer!");
+                });
             }
         } else {
             console.log(`Card ${name} of type ${type} already exists.`);
