43a5c87a216bfdc11febfc78d2d50198998577d8
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:17:19 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -29,7 +29,7 @@ function addCard() {
 
     switch (rarity) {
         case "Common":
-            if (["Person", "Normal"].includes)
+            if (["Person", "Normal"].includes())
             break;
         case "Normal":
             break;
