363cfcea3d52ebe709837077a607c3e601d71f79
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 13:32:47 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -32,7 +32,7 @@ function addCard() {
 
     var reader = new FileReader();
     reader.onloadend = function () {
-        return console.log(reader.re);
+        return console.log(reader.readAsDataURL);
     }
 
     if (thumbnail) {
