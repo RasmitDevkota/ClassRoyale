7c40d973a7000e52236bd7c0b1a3a8999c1ae973
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 11:37:46 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -17,8 +17,6 @@ var emails = db.collection("emails");
 
 // Get a reference to the storage service, which is used to create references in your storage bucket
 var storage = firebase.storage();
-
-// Create a storage reference from our storage service
 var storageRef = storage.ref();
 
 function addCard() {
