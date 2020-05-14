7d84a30ddda4a6c8fd4758474ed3016c601cc125
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 14:23:56 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 firebase.initializeApp({
     apiKey: "AIzaSyDz4E7cWDIMedScuc4TvGdEawhWGOLn4SQ",
-    authDomain: "classroyale.firebaseapp.com",
+    authDomain: "class-royale.firebaseapp.com",
     databaseURL: "https://class-royale.firebaseio.com",
     projectId: "class-royale",
     storageBucket: "class-royale.appspot.com",
