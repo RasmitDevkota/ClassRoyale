4eab4116a419ba84aeb4e4c6c52807ecc429db78
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:18:59 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -26,7 +26,8 @@ function addCard() {
     var thumbnail = document.getElementById("cardThumbnail");
 
     var seed = Math.round(Math.random());
-    var HPValues = [100,150,200,250,300,350,400,450,500,550,600]
+    var HPValues = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
+    var attackDamageValues = []
 
     switch (rarity) {
         case "Common":
