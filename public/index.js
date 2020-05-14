4bc3e42c4472d9c4570691d339691d7050ded5a3
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 13:07:18 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -19,7 +19,7 @@ var storage = firebase.storage().ref();
 var cardThumbnails = storage.child('card_thumbnails')
 
 function addCard() {
-    var name = document.getElementById("cardName").valu;
+    var name = document.getElementById("cardName").value;
     var description = document.getElementById("cardDescription").text;
 
     var type = $("cardType").val();
