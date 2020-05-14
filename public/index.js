a58dbcafee1db3b68f4ed7ea6b40f6f403c920cc
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:20:46 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -43,8 +43,9 @@ function addCard() {
             }
             break;
         case "Rare":
+            var HP = HPValues[seed + 2];
             if (["Person", "Normal"].includes(type)) {
-
+                var attackDamage = attackDamageValues[seed + 2];
             }
             break;
         case "Ultra Rare":
