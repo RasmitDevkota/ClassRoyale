f571adc133138eb1ff2930892522451fad6c7df6
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:01:24 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -20,7 +20,6 @@ var cardThumbnails = storage.child('card_thumbnails')
 
 function addCard() {
     var name = document.getElementById("cardName").textContent;
-    var HP = document.getElementById("cardHP").textContent;
     var type = document.getElementById("cardType").textContent;
     var description = document.getElementById("cardDescription").textContent;
     var rarity = document.getElementById("cardRarity").textContent;
