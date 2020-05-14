8c59d242133dfcdef8236bd436898f51e08ac7f5
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 14:43:24 -0400

    Gitacora saved 3 changed files

--- public/index.js
+++ public/index.js
@@ -35,6 +35,8 @@ function addCard() {
         var HPValues = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600];
         var attackDamageValues = [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80];
 
+        
+
         switch (rarity) {
             case "Common":
                 var HP = HPValues[seed];
