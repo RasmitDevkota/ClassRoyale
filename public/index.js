144580b9c6b8217a4a7faa275402a81aa31fe52d
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 14:44:34 -0400

    Gitacora saved 3 changed files

--- public/index.js
+++ public/index.js
@@ -34,7 +34,7 @@ function addCard() {
     var HPValues = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600];
     var attackDamageValues = [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80];
     var seedOffsets = new Map([
-        ['foo', 3],
+        ['Common', 3],
         ['bar', {}],
         ['baz', undefined]
     ]);
