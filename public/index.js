cc6238ba1166ff015cdf5ceb24a0b34a1c002df4
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-14 12:20:33 -0400

    Gitacora saved 1 changed files

--- public/index.js
+++ public/index.js
@@ -37,7 +37,7 @@ function addCard() {
             }
             break;
         case "Normal":
-            var HP = HPValues[seed];
+            var HP = HPValues[seed+2];
             if (["Person", "Normal"].includes(type)) {
                 var attackDamage = attackDamageValues[seed];
             }
