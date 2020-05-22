9dc6f5824668f2e2b0c0fe47348e8d90000f9152
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-22 13:27:33 -0400

    Gitacora saved 11 changed files

--- public/server.js
+++ public/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 const botName = 'ChatCord Bot';
 
