86f21f846700555269fe135ffeb73bf3ab655756
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-26 10:00:50 -0400

    Gitacora saved 13 changed files

--- public/index.js
+++ public/index.js
@@ -146,7 +146,7 @@ function testGame(name1, name2) {
 }
 
 function startGame(name) {
-    queues.equlimitToFirst(1);
+    queues.equalTo.limitToFirst(1);
 
     return;
     emails.doc(name).get().then(function (doc) {
