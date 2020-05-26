9a7aee368876986060aafe73be1551ec47ccc0b3
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-26 10:18:31 -0400

    Gitacora saved 13 changed files

--- public/index.js
+++ public/index.js
@@ -146,7 +146,11 @@ function testGame(name1, name2) {
 }
 
 function startGame(name) {
-    var queue = queues.push();
+    var queue = queues.push(
+
+        "status": "PENDING",
+        "user1": name
+    );
 
     queue.set({
     });
