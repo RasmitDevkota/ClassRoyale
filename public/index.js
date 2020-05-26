712deffc40575b1a5446f49a0a9f3c86492ee032
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-26 10:53:23 -0400

    Gitacora saved 13 changed files

--- public/index.js
+++ public/index.js
@@ -174,7 +174,7 @@ function startGame(name) {
 }
 
 function joinGame(name) {
-    var gameToJoin = queues.orderByValue().equalTo("PENDING", "queue").limitToFirst(1)..once('value', function (snapshot) {
+    var gameToJoin = queues.orderByValue().equalTo("PENDING", "queue").limitToFirst(1).once('value', function (snapshot) {
         snapshot.forEach(function (childSnapshot) {
             var childKey = childSnapshot.key;
             var childData = childSnapshot.val();
