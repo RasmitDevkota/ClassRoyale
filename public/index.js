2557ffd8e74de22c51882d1abdc1c39f85dac1c5
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-26 10:08:18 -0400

    Gitacora saved 13 changed files

--- public/index.js
+++ public/index.js
@@ -166,7 +166,7 @@ function startGame(name) {
                 "title": "Using Android Architecture Components with Firebase Realtime Database (Part 3)",
                 "author": "Doug",
             }
-        },
+        }
     });
 
     queues.orderByKey().equalTo("PENDING", "QUEUE").limitToFirst(1).on('value', function (snapshot) {
