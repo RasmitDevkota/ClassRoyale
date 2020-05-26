12f6cc3223033fec39c9241a9ccd6d304134b8df
Dr AlienTech <alientechgamig@hotmail.com>
2020-05-26 10:49:46 -0400

    Gitacora saved 13 changed files

--- public/index.js
+++ public/index.js
@@ -174,7 +174,7 @@ function startGame(name) {
 }
 
 function joinGame(/* gameId, */name) {
-    var gameToJoin = queues.orderByValue().equalTo("PENDING", "queue").limitToFirst(1).push().key;
+    var gameToJoin = queues.orderByValue().equalTo("PENDING", "queue").limitToFirst(1).key;
 
     var update = {};
     update['/queues/' + gameToJoin + '/queue/' + name] = firebase.firestore.FieldValue.serverTimestamp();
