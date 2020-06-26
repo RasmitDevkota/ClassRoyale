620d3eb679729cd8f93ee5358539a5599f9681b1
Dr AlienTech <alientechgamig@hotmail.com>
2020-06-26 14:03:12 -0400

    Gitacora saved 17 changed files

--- public/index.js
+++ public/index.js
@@ -176,7 +176,9 @@ function startGame(name) {
 
                             console.log(queueList);
                         });
-                    }).then(function());
+                    }).then(function () {
+                        
+                    });
 
                     var sortedQueueList = new Map([...queueList.entries()].sort((a, b) => b[1] - a[1]));
                     var chosenUser = sortedQueueList.keys().next().value;
