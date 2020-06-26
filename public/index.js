76d8985523a43a955c154ee5863ce6ca43137b62
Dr AlienTech <alientechgamig@hotmail.com>
2020-06-26 14:40:48 -0400

    Gitacora saved 16 changed files

--- public/index.js
+++ public/index.js
@@ -211,7 +211,7 @@ function joinGame(name) {
             firebase.database().ref().update(update).then(function () {
                 console.log("Found a game!");
 
-                var i = 0;
+                var j = 0;
                 queues.child(gameKey).on('child_added', function () {
                     if (i != 3) {
                         i++;
