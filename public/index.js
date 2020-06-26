d3a14065f4c9fcac65357dfb4b4be7bc5e367ad4
Dr AlienTech <alientechgamig@hotmail.com>
2020-06-26 14:23:44 -0400

    Gitacora saved 17 changed files

--- public/index.js
+++ public/index.js
@@ -158,7 +158,7 @@ function startGame(name) {
         
         var i = 0;
         queue.on('child_added', function (data) {
-            if (i < 2) {
+            if (i < 3) {
                 i++;
                 console.log(i);
             } else {
