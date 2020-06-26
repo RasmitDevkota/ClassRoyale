edde160604e84a4fa51f87e76159d243af63ea28
Dr AlienTech <alientechgamig@hotmail.com>
2020-06-26 13:46:46 -0400

    Gitacora saved 17 changed files

--- public/index.js
+++ public/index.js
@@ -182,7 +182,7 @@ function startGame(name) {
 
                     queue.update({
                         
-                    })
+                    });
                 });
             }
         });
