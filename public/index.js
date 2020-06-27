dc8079c62a4d122770622fedbf29dd94abc4236c
Dr AlienTech <alientechgamig@hotmail.com>
2020-06-27 09:35:18 -0400

    Gitacora saved 19 changed files

--- public/index.js
+++ public/index.js
@@ -210,7 +210,7 @@ function joinGame(name) {
                     } else {
                         queues.child(gameKey).once('value', function (data) {
                             console.log("userStatus,CHOSEN");
-                            console.log("mine,CHOSEN");
+                            console.log("mine,false");
                             console.log(`joinedUser,${data.val()["user2"]}`);
                         });
                     }
